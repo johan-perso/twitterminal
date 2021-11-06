b6ddc8934f1a99b69fa6b97e5fbb72f360188dd1
Johan <41506568+johan-perso@users.noreply.github.com>
2021-11-06 19:26:14 +0100

    Update gif.js

--- functions/gif.js
+++ functions/gif.js
@@ -1,14 +1,12 @@
-// Importer une fonction locale
-var fetch = require('node-fetch')
+// Importer node-fetch
+const fetch = require('node-fetch')
 
 // Exporter en tant que module
 module.exports = async function(text){
 	// Faire une requête vers tenor
-	var gif = await fetch(`https://api.tenor.com/v1/search?q=${text}&key=LIVDSRZULELA&limit=1`, {
-        method: 'get'
-    })
+	var gif = await fetch(`https://api.tenor.com/v1/search?q=${text}&key=LIVDSRZULELA&limit=1`)
     .then(res => res.json())
-	.catch(err => {})
+	.catch(err => { return "" })
 
 	// Retourner le lien du gif
 	if(gif && gif.results && gif.results[0]) return gif.results[0].url; else return ""
