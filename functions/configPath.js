5eb763c0195aeaf3685d601d6cc5ca10fe98b0a0
Johan le stickman <41506568+johan-perso@users.noreply.github.com>
2021-12-15 14:23:19 +0100

    Create configPath.js

---
+++ functions/configPath.js
@@ -0,0 +1,8 @@
+module.exports = function (jsonExtension=true){
+	if(require('os').platform() === "win32") var configPath = require('path').join(process.env.APPDATA, "johanstickman-cli", "twitterminal")
+	if(require('os').platform() === "darwin") var configPath = require('path').join(require('os').homedir(), "library", "Preferences", "johanstickman-cli", "twitterminal")
+	if(require('os').platform() === "linux") var configPath = require('path').join(require('os').homedir(), ".config", "johanstickman-cli", "twitterminal")
+
+	if(jsonExtension === true) configPath = require('path').join(configPath, "twitterminalConfig.json")
+	return configPath;
+}
