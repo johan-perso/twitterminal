bbf00f62ae2c8e011b8a3200badb1fa8fda20f0f
Johan le stickman <Johan_Stickman@proton.me>
2022-07-13 16:00:14 +0200

    Update gif.js

--- functions/gif.js
+++ functions/gif.js
@@ -1,7 +1,7 @@
 // Exporter en tant que module
 module.exports = async function(text){
 	// Faire une requête vers tenor
-	var gif = await require('node-fetch')(`https://api.tenor.com/v1/search?q=${text}&key=LIVDSRZULELA&limit=1`)
+	var gif = await require('node-fetch')(`https://api.tenor.com/v1/search?q=${text}&key=LIVDSRZULELA&limit=1`) // pov : c'est la clé qu'ils ont mit dans l'exemple sur leur documentation, sauf qu'elle marche vraiment
 	.then(res => res.json())
 	.catch(err => { return "" })
 
